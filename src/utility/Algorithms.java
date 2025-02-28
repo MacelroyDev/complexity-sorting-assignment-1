321028e897160d5b88395d3f7a3523327777232b
Ethan Van De Woestyne <ethan.van@protonmail.com>
2025-02-27 20:51:10 -0700

    Fixes sorting messaging

--- src/utility/Algorithms.java
+++ src/utility/Algorithms.java
@@ -7,45 +7,14 @@ public class Algorithms
 	public static Shape[] bubbleSort(Shape[] shapes, char unitType)
 	{
 		int n = shapes.length;
-		int comparisonNum = 0;
+		int sortedCount = 1;
 
 		System.out.println("First element is: " + shapes[0].getClass() + (unitType == 'h'
-				? " Height: " + shapes[0].getHeight() : unitType == 'v' ? " Volume: " + 
-						shapes[0].calcVolume() : " Area: " + shapes[0].calcBaseArea()));
+				? " Height: " + shapes[0].getHeight()
+				: unitType == 'v' ? " Volume: " + shapes[0].calcVolume() : " Area: " + shapes[0].calcBaseArea()));
 
 		for (int i = 0; i < n - 1; i++)
 		{
-			comparisonNum++;
-			if (comparisonNum % 1000 == 0)
-			{
-				int thousandthNum = comparisonNum / 1000;
-				if (unitType == 'h')
-				{
-					System.out.println(thousandthNum + "000-th element: " + 
-							shapes[i].getClass() + " Height: " 
-							+ shapes[i].getHeight());
-				}
-				else if (unitType == 'v')
-				{
-					System.out.println(thousandthNum + "000-th element: " + 
-							shapes[i].getClass() + " Volume: " 
-							+ shapes[i].calcVolume());
-				}
-				else
-				{
-					System.out.println(thousandthNum + "000-th element: " + 
-							shapes[i].getClass() + " Area: " 
-							+ shapes[i].calcBaseArea());
-				}
-			}
-			
-			if (comparisonNum == n)
-			{
-				System.out.println("Last element is: " + shapes[n-1].getClass() + (unitType == 'h'
-						? " Height: " + shapes[n-1].getHeight() : unitType == 'v' ? " Volume: " + 
-								shapes[n-1].calcVolume() : " Area: " + shapes[n-1].calcBaseArea()));
-			}
-			
 			for (int x = 0; x < n - i - 1; x++)
 			{
 				if (unitType == 'h')
@@ -66,7 +35,7 @@ public class Algorithms
 						shapes[x + 1] = temp;
 					}
 				}
-				else
+				else if (unitType == 'a')
 				{
 					if (shapes[x].calcBaseArea() < shapes[x + 1].calcBaseArea())
 					{
@@ -76,52 +45,35 @@ public class Algorithms
 					}
 				}
 			}
+			sortedCount++;
+			if (sortedCount % 1000 == 0)
+			{
+				System.out.println(sortedCount + "-th element: " + shapes[n - i - 1].getClass()
+						+ (unitType == 'h' ? " Height: " + shapes[n - i - 1].getHeight()
+								: unitType == 'v' ? " Volume: " + shapes[n - i - 1].calcVolume()
+										: " Area: " + shapes[n - i - 1].calcBaseArea()));
+			}
+
 		}
+		System.out.println("Last element is: " + shapes[n - 1].getClass()
+				+ (unitType == 'h' ? " Height: " + shapes[n - 1].getHeight()
+						: unitType == 'v' ? " Volume: " + shapes[n - 1].calcVolume()
+								: " Area: " + shapes[n - 1].calcBaseArea()));
+
 		return shapes;
 	}
 
 	public static Shape[] insertionSort(Shape[] shapes, char unitType)
 	{
 		int n = shapes.length;
-		int comparisonNum = 0;
-		
+		int sortedCount = 1;
+
 		System.out.println("First element is: " + shapes[0].getClass() + (unitType == 'h'
-				? " Height: " + shapes[0].getHeight() : unitType == 'v' ? " Volume: " + 
-						shapes[0].calcVolume() : " Area: " + shapes[0].calcBaseArea()));
-		
+				? " Height: " + shapes[0].getHeight()
+				: unitType == 'v' ? " Volume: " + shapes[0].calcVolume() : " Area: " + shapes[0].calcBaseArea()));
+
 		for (int i = 1; i < n; i++)
 		{
-			comparisonNum++;
-			if (comparisonNum % 1000 == 0)
-			{
-				int thousandthNum = comparisonNum / 1000;
-				if (unitType == 'h')
-				{
-					System.out.println(thousandthNum + "000-th element: " + 
-							shapes[i].getClass() + " Height: " 
-							+ shapes[i].getHeight());
-				}
-				else if (unitType == 'v')
-				{
-					System.out.println(thousandthNum + "000-th element: " + 
-							shapes[i].getClass() + " Volume: " 
-							+ shapes[i].calcVolume());
-				}
-				else
-				{
-					System.out.println(thousandthNum + "000-th element: " + 
-							shapes[i].getClass() + " Area: " 
-							+ shapes[i].calcBaseArea());
-				}
-			}
-			
-			if (comparisonNum == n)
-			{
-				System.out.println("Last element is: " + shapes[0].getClass() + (unitType == 'h'
-						? " Height: " + shapes[0].getHeight() : unitType == 'v' ? " Volume: " + 
-								shapes[0].calcVolume() : " Area: " + shapes[0].calcBaseArea()));
-			}
-			
 			Shape index = shapes[i];
 			int j = i - 1;
 
@@ -151,52 +103,35 @@ public class Algorithms
 			}
 
 			shapes[j + 1] = index;
+			sortedCount++;
+			if (sortedCount % 1000 == 0)
+			{
+				System.out.println(sortedCount + "-th element: " + shapes[i].getClass()
+						+ (unitType == 'h' ? " Height: " + shapes[i].getHeight()
+								: unitType == 'v' ? " Volume: " + shapes[i].calcVolume()
+										: " Area: " + shapes[i].calcBaseArea()));
+			}
 		}
+
+		System.out.println("Last element is: " + shapes[n - 1].getClass()
+				+ (unitType == 'h' ? " Height: " + shapes[n - 1].getHeight()
+						: unitType == 'v' ? " Volume: " + shapes[n - 1].calcVolume()
+								: " Area: " + shapes[n - 1].calcBaseArea()));
+
 		return shapes;
 	}
 
 	public static Shape[] selectionSort(Shape[] shapes, char unitType)
 	{
 		int n = shapes.length;
-		int comparisonNum = 1;
-		
+		int sortedCount = 1;
+
 		System.out.println("First element is: " + shapes[0].getClass() + (unitType == 'h'
-				? " Height: " + shapes[0].getHeight() : unitType == 'v' ? " Volume: " + 
-						shapes[0].calcVolume() : " Area: " + shapes[0].calcBaseArea()));
-		
+				? " Height: " + shapes[0].getHeight()
+				: unitType == 'v' ? " Volume: " + shapes[0].calcVolume() : " Area: " + shapes[0].calcBaseArea()));
+
 		for (int i = 0; i < n - 1; i++)
 		{
-			comparisonNum++;
-			if (comparisonNum % 1000 == 0)
-			{
-				int thousandthNum = comparisonNum / 1000;
-				if (unitType == 'h')
-				{
-					System.out.println(thousandthNum + "000-th element: " + 
-							shapes[i].getClass() + " Height: " 
-							+ shapes[i].getHeight());
-				}
-				else if (unitType == 'v')
-				{
-					System.out.println(thousandthNum + "000-th element: " + 
-							shapes[i].getClass() + " Volume: " 
-							+ shapes[i].calcVolume());
-				}
-				else
-				{
-					System.out.println(thousandthNum + "000-th element: " + 
-							shapes[i].getClass() + " Area: " 
-							+ shapes[i].calcBaseArea());
-				}
-			}
-			
-			if (comparisonNum == n)
-			{
-				System.out.println("Last element is: " + shapes[0].getClass() + (unitType == 'h'
-						? " Height: " + shapes[0].getHeight() : unitType == 'v' ? " Volume: " + 
-								shapes[0].calcVolume() : " Area: " + shapes[0].calcBaseArea()));
-			}
-			
 			int maxIndex = i;
 
 			for (int j = i + 1; j < n; j++)
@@ -223,10 +158,27 @@ public class Algorithms
 					}
 				}
 			}
+
 			Shape temp = shapes[i];
 			shapes[i] = shapes[maxIndex];
 			shapes[maxIndex] = temp;
+
+			sortedCount++;
+			if (sortedCount % 1000 == 0)
+			{
+				System.out.println(sortedCount + "-th element: " + shapes[i].getClass()
+						+ (unitType == 'h' ? " Height: " + shapes[i].getHeight()
+								: unitType == 'v' ? " Volume: " + shapes[i].calcVolume()
+										: " Area: " + shapes[i].calcBaseArea()));
+			}
 		}
+
+		System.out.println("Last element is: " + shapes[n - 1].getClass()
+				+ (unitType == 'h' ? " Height: " + shapes[n - 1].getHeight()
+						: unitType == 'v' ? " Volume: " + shapes[n - 1].calcVolume()
+								: " Area: " + shapes[n - 1].calcBaseArea()));
+
 		return shapes;
 	}
+
 }
